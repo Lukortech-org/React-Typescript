262fd101534f5f4b6b5f7c237180deca5f935e26
Lukasz Formela <lukasz.formela@hpe.com>
2022-10-02 03:33:25 +0200

    chore(formatting): now should run linter and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,32 @@
 import { createServer, Model } from "miragejs"
 
 export default function makeServer() {
-    const createUser =(name:string)=>{
-        return{
-            name
-        }
+  const createUser = (name: string) => {
+    return {
+      name,
     }
-    createServer({
+  }
+  createServer({
+    models: {
+      user: Model,
+    },
 
-        models: {
-            user:Model,
-        },
+    logging: true,
 
-        logging: true,
+    seeds(server) {
+      //@ts-ignore
+      server.create("user", createUser("John Doe"))
+      //@ts-ignore
+      server.create("user", createUser("John Smith"))
+      //@ts-ignore
+      server.create("user", createUser("John Buck"))
+    },
 
-        seeds(server) {
-            //@ts-ignore
-            server.create("user", createUser("John Doe"))
-            //@ts-ignore
-            server.create("user", createUser("John Smith"))
-            //@ts-ignore
-            server.create("user", createUser("John Buck"))
-        },
+    routes() {
+      this.namespace = "api"
 
-        routes() {
-            this.namespace = "api"
-
-            //@ts-ignore
-            this.get("/users", (schema) => schema.users.all())
-        },
-    })
+      //@ts-ignore
+      this.get("/users", (schema) => schema.users.all())
+    },
+  })
 }
