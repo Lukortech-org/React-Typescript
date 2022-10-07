b3cccc4db9842254db24e7932f872adfd62e73c9
Qubaka <kozikuba204@gmail.com>
2022-10-07 13:42:11 +0200

    added select, still some bugs

--- src/pages/Table/Table.tsx
+++ src/pages/Table/Table.tsx
@@ -93,6 +93,13 @@ export default function BasicTable() {
             getapi('api/users/'+page+'/'+limit)
         }
       }}>Next</button>
+      <select name="limit" id="limit" onChange={(e)=>{
+        limit = Number(e.target.value);
+      }}>
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="25">25</option>
+      </select>
     </TableContainer>
   );
 }
