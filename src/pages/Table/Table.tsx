02a14cff6cf43692ba61dc0a5102b3bc0003eb38
Lukasz Formela <lukasz.formela@hpe.com>
2022-10-12 16:51:53 +0200

    chore(formatted): had to ran format one last time

--- src/pages/Table/Table.tsx
+++ src/pages/Table/Table.tsx
@@ -1,64 +1,64 @@
-import Ract, { useState, useEffect } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import UserI from "../../types/User";
+import React, { useState, useEffect } from "react"
+import Table from "@mui/material/Table"
+import TableBody from "@mui/material/TableBody"
+import TableCell from "@mui/material/TableCell"
+import TableContainer from "@mui/material/TableContainer"
+import TableHead from "@mui/material/TableHead"
+import TableRow from "@mui/material/TableRow"
+import Paper from "@mui/material/Paper"
+import UserI from "../../types/User"
 
 interface ParamsI {
-  page: number;
-  limit: number;
+  page: number
+  limit: number
 }
 
 interface DataI {
-  users: UserI[];
-  totalEntries: number;
+  users: UserI[]
+  totalEntries: number
 }
 
 export default function BasicTable() {
   const initialDataState: DataI = {
     users: [],
     totalEntries: 0,
-  };
-  const [data, setData] = useState<DataI>(initialDataState);
+  }
+  const [data, setData] = useState<DataI>(initialDataState)
   const [params, setParams] = useState<ParamsI>({
     page: 0,
     limit: 10,
-  });
+  })
 
   useEffect(() => {
     async function getUsers(url: string, params?: ParamsI) {
-      const response = await fetch(url + params?.page + "/" + params?.limit);
-      const { users, totalEntries } = await response.json();
+      const response = await fetch(url + params?.page + "/" + params?.limit)
+      const { users, totalEntries } = await response.json()
 
-      setData({ users: users.models, totalEntries: totalEntries });
+      setData({ users: users.models, totalEntries: totalEntries })
     }
 
-    getUsers("api/users/", params);
-  }, [params]);
+    getUsers("api/users/", params)
+  }, [params])
 
   const handlePreviousPage = () => {
     if (params.page > 0) {
-      setParams((prevState) => ({ ...prevState, page: prevState.page - 1 }));
+      setParams((prevState) => ({ ...prevState, page: prevState.page - 1 }))
     }
-  };
+  }
 
   const handleNextPage = () => {
     if (params.page < Math.ceil(data.totalEntries / params.limit - 1)) {
-      setParams((prevState) => ({ ...prevState, page: prevState.page + 1 }));
+      setParams((prevState) => ({ ...prevState, page: prevState.page + 1 }))
     }
-  };
+  }
 
   const handleLimitChange = (e: { target: { value: string } }) => {
     setParams((prevState) => ({
       ...prevState,
       limit: Number(e.target.value),
       page: 0,
-    }));
-  };
+    }))
+  }
 
   return (
     <TableContainer component={Paper}>
@@ -100,5 +100,5 @@ export default function BasicTable() {
         <option value="25">25</option>
       </select>
     </TableContainer>
-  );
+  )
 }
