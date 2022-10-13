16cd523db61e45d385cb8f7bb745522f42e61502
Qubaka <kozikuba204@gmail.com>
2022-10-13 13:35:32 +0200

    minor fixes 2 eh

--- src/pages/Table/Table.tsx
+++ src/pages/Table/Table.tsx
@@ -7,7 +7,6 @@ import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
 import Paper from "@mui/material/Paper"
 import UserI from "../../types/User"
-import "./table.css"
 
 interface ParamsI {
   page: number
